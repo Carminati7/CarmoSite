af767779ef9c6077f00f245a00db843013b54a92
carminati7 <stefano.carminati7@gmail.com>
2021-02-05 23:08:41 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { GifRandomizerComponent } from './gif-randomizer/gif-randomizer.component';
-// import { ScullyLibModule } from '@scullyio/ng-lib-v8';
-// import { ScullyLibModule } from '@scullyio/ng-lib';
+import { ScullyLibModule } from '@scullyio/ng-lib-v8';
 
 @NgModule({
   declarations: [
@@ -17,8 +16,8 @@ import { GifRandomizerComponent } from './gif-randomizer/gif-randomizer.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-     HttpClientModule
-    // ScullyLibModule
+     HttpClientModule,
+     ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
