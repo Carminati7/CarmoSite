74b3766c56dffdf4924e56b3bf072f874974b77a
carminati7 <stefano.carminati7@gmail.com>
2021-02-05 22:52:19 +0100

    testing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('avigliana-mtg app is running!');
+    expect(page.getTitleText()).toEqual('CarmoSite app is running!');
   });
 
   afterEach(async () => {
