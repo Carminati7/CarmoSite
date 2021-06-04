2869f91f9c408ea1cf104f77c02c3e3c3acf27e2
Carminati7 <stefano.carminati7@gmail.com>
2021-06-04 14:48:46 +0200

    .

--- src/app/poster/poster.component.ts
+++ src/app/poster/poster.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-poster',
   templateUrl: './poster.component.html',
   styleUrls: ['./poster.component.scss']
 })
-export class PosterComponent implements OnInit {
+export class PosterComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('poster') poster
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    $('#poster').turn({
+  		width: 400,
+  		height: 300,
+  		autoCenter: true
+  	});
   }
 
 }
