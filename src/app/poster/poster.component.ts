85a93944d3b3ff688542f976b4a8d7631d62e409
Carminati7 <stefano.carminati7@gmail.com>
2021-06-04 18:26:04 +0200

    .

--- src/app/poster/poster.component.ts
+++ src/app/poster/poster.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  HostListener
+} from '@angular/core';
 
 @Component({
   selector: 'app-poster',
@@ -7,19 +13,45 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 })
 export class PosterComponent implements OnInit, AfterViewInit {
 
-  @ViewChild('poster') poster
-
+  @ViewChild('poster1') poster1
+  @ViewChild('poster2') poster2
+  @ViewChild('poster3') poster3
+  @ViewChild('poster4') poster4
+  poster = 4
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(): void {
-    $('#poster')['turn']({
+  @HostListener('window:resize', ['$event'])
+
+  adjustAllSize() {
+    for( let i = 1 ; i <= this.poster; i++) {
+      this.adjustSize( 'poster' + i )
+    }
+  }
+
+  adjustSize( name ) {
+    if ( window.innerWidth < 600) {
+      $('#' + name )['turn']("size",'88vw','20vh');
+    } else {
+      $('#' + name)['turn']("size",'70vw ','20vh');
+    }
+  }
+
+  initPoster( name: string ) {
+    $('#'+name)['turn']({
       acceleration: true,
-  		autoCenter: true,
+      autoCenter: true,
       display: 'single'
-  	});
+    });
+    this.adjustSize( name )
+  }
+
+  ngAfterViewInit(): void {
+    for( let i = 1 ; i <= this.poster; i++) {
+      this.initPoster('poster' + i)
+    }
   }
 
 }
